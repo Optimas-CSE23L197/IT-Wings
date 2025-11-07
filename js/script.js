8c0d9b1169eeda72017f30fd4b3d9190c54a1f19
Animesh <karananimesh144@gmail.com>
2025-11-07 16:20:50 +0530

    Add HTML structure and styles for About, Contact, Course, E-Learning, and Service pages; implement responsive navigation and service statistics section.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// navbar toggle
+const humberger = document.querySelector(".hamburger i");
+let navMenu = document.querySelector(".nav-menu");
+
+humberger.addEventListener("click", function () {
+  navMenu.classList.toggle("show-menu");
+  if (navMenu.classList.contains("show-menu")) {
+    humberger.classList.remove("fa-bars");
+    humberger.classList.add("fa-xmark");
+  } else {
+    humberger.classList.remove("fa-xmark");
+    humberger.classList.add("fa-bars");
+  }
+});
+
 // service active navigation
 let serviceNavigation = document.querySelectorAll(".show-service-list ul li a");
 
@@ -33,3 +48,24 @@ function filterService(category) {
     service.style.display = "block";
   });
 }
+
+// // service search
+// let searchInput = document.querySelector(".service-search");
+// let serviceCards = document.querySelectorAll(".service-card");
+// console.log(serviceCards);
+
+// searchInput.addEventListener("input", function () {
+//   const searchItem = this.value.toLowerCase().trim();
+//   console.log(searchItem);
+
+//   serviceCards.forEach((card) => {
+//     const title = card.querySelector("h3").textContent.toLowerCase();
+//     const description = card.querySelector("p").textContent.toLowerCase();
+
+//     if (title.includes(searchInput) || description.includes(searchInput)) {
+//       card.parentElement.style.display = "";
+//     } else {
+//       card.parentElement.style.display = "none";
+//     }
+//   });
+// });
