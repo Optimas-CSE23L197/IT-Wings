852cb39fd29851520dbc39ea294c7375dfa55727
Animesh <karananimesh144@gmail.com>
2025-11-11 22:54:51 +0530

    fix landing page

--- js/main.js
+++ js/main.js
@@ -180,10 +180,7 @@ navContact.addEventListener("click", (e) => {
   serviceSection.classList.remove("active");
 });
 
-
 /*.............New Career..........*/
 console.log("Hero section loaded successfully!");
 console.log("Cards loaded successfully!");
 console.log("Deloitte people section loaded successfully!");
-
-
